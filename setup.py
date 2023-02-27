9c28533ebf17699c8987abe23f2e10b088bd812e
Quinten <quinten.roets@gmail.com>
2023-02-27 13:00:06 -0500

    migrate setup to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-NAME = "cacher"
-
-
-def read(filename):
-    try:
-        with open(filename) as fp:
-            content = fp.read().split("\n")
-    except FileNotFoundError:
-        content = []
-    return content
-
-
-setup(
-    author="Quinten Roets",
-    author_email="quinten.roets@gmail.com",
-    description="function caching",
-    name=NAME,
-    version="1.0",
-    packages=find_packages(),
-    install_requires=read("requirements.txt"),
-    entry_points={"console_scripts": ["clearcache = cacher.clearing:main"]},
-)
+if __name__ == "__main__":
+    setup()
